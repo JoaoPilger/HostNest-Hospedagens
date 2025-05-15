989ecc0a936055cb330bbe636613882f5d24e80d
shinigami303 <mikael.prime303@gmail.com>
2025-05-15 15:41:20 -0300

    começo do frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./paginas/home";
 import Cadastro from "./paginas/cadastro";
+import Casa from "./paginas/casas";
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/" element={<Home/>} />
+        <Route path="/cadastro" element={<Cadastro/>} />
+        <Route path="/casa:id" element={<Casa/>} />
       </Routes>
     </Router>
   )
---
+++ frontend/src/paginas/casas.jsx
@@ -0,0 +1,29 @@
+
+
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import casas from './data/casas.json';
+
+export default function Casa() {
+  const { id } = useParams();
+  const [casa, setCasa] = useState(null);
+
+  useEffect(() => {
+    
+    const encontrada = casas.find(c => c.id === id);
+    setCasa(encontrada);
+  }, [id]);
+
+  
+  if (!casa) return <p>Casa não encontrada.</p>;
+
+  
+  return (
+    <div>
+      <h1>{casa.titulo}</h1>
+      <img src={casa.imagem} alt={casa.titulo} width="400" />
+      <p>{casa.descricao}</p>
+      <p><strong>Preço por noite:</strong> R$ {casa.preco}</p>
+    </div>
+  );
+}
