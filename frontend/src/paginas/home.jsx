6d3413ef7174a3c25770af6cf977529147471d8e
Qlqr1 <guto.kg2008@gmail.com>
2025-05-20 09:00:51 -0300

    ajeitei o header

--- frontend/src/paginas/home.jsx
+++ frontend/src/paginas/home.jsx
@@ -15,26 +15,28 @@ export default function Home(){
             </header>
             <section id="escolher">
                 <h2>Escolha sua estadia</h2>
-                <div className="barra-pesquisa">
+                <div className='linha'>
                     <button>
                         <img src="" alt="Imagem de Filtros " />
                         Filtros
                     </button>
-                    <div className="input-group">
-                        <label htmlFor="local">Local</label>
-                        <input type="text" id='local' />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="check-in">Check-in</label>
-                        <input type="date" id="check-in" />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="check-out">Check-out</label>
-                        <input type="date" id="check-out" />
-                    </div>
-                    <div className="input-group">
-                        <label htmlFor="quantidade">Quantos hóspedes?</label>
-                        <input type="number" min="1" id="quantidade" />
+                    <div className="barra-pesquisa">     
+                        <div className="input-group">
+                            <label htmlFor="local">Local</label>
+                            <input type="text" id='local' />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="check-in">Check-in</label>
+                            <input type="date" id="check-in" />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="check-out">Check-out</label>
+                            <input type="date" id="check-out" />
+                        </div>
+                        <div className="input-group">
+                            <label htmlFor="quantidade">Quantos hóspedes?</label>
+                            <input type="number" min="1" id="quantidade" />
+                        </div>
                     </div>
                     <button>Buscar</button>
                 </div>
