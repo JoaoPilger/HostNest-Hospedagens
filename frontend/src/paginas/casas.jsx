8196d6965b7da78e551e4311cb6bfc34be8dcde3
Qlqr1 <guto.kg2008@gmail.com>
2025-08-19 22:50:29 -0300

    Transferi a reserva para a mesma página

--- frontend/src/paginas/casas.jsx
+++ frontend/src/paginas/casas.jsx
@@ -1,29 +1,130 @@
-
-
-import { useParams, Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState, useMemo } from 'react';
 import casas from './data/casas.json';
+import "../styles/cadastro.css";
 
 export default function Casa() {
   const { id } = useParams();
   const [casa, setCasa] = useState(null);
+  const [registroData, setRegistroData] = useState({
+    hospedes: 1,
+    dataIni: "",
+    dataFim: "",
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegistroData(prev => ({
+      ...prev,
+      [name]: name === "hospedes" ? Number(value) : value
+    }));
+    
+    if (errors[name]) {
+      setErrors(prev => ({
+        ...prev,
+        [name]: ''
+      }));
+    }
+  };
 
   useEffect(() => {
     const encontrada = casas.find(c => c.id === Number(id));
     setCasa(encontrada);
   }, [id]);
 
+  function quantasNoites(inicio, fim) {
+    if (!inicio || !fim) return 1;
+    const inicioData = new Date(inicio);
+    const fimData = new Date(fim);
+    const diferenca = Math.ceil((fimData - inicioData) / (1000 * 60 * 60 * 24)); // Pelo que vi o Date tranforma em milissegundos, então (1000 * 60 * 60 * 24) transforma em dias
+    return diferenca > 0 ? diferenca : 1;
+  }
+
+  const validateForm = () => {
+    const newErrors = {};
+
+    if (!registroData.dataIni) {
+      newErrors.dataIni = 'Check-in é obrigatório';
+    } else if (!registroData.dataFim){
+      newErrors.dataFim = 'Check-out é obrigatório';
+    } else if(new Date(registroData.dataFim) <= new Date(registroData.dataIni)){
+      newErrors.dataFim = 'Check-out deve ser depois do Check-in';
+    }
+
+    return newErrors;
+  };
+
+  const handleSubmit = async (e) => {  
+    e.preventDefault();
+    const newErrors = validateForm();
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+      return;
+    }
+
+    // Precisa adicionar mais
+  };
 
   if (!casa) return <p>Casa não encontrada.</p>;
 
+  const periodo = quantasNoites(registroData.dataIni, registroData.dataFim);
+
+  const precoFinal = casa.preco * periodo;
 
   return (
     <div>
-    <h1>{casa.titulo}</h1>
-    <img src={casa.imagem} alt={casa.titulo} width="400" />
+      <h1>{casa.titulo}</h1>
+      <img src={casa.imagem} alt={casa.titulo} width="400" />
       <p>{casa.descricao}</p>
-      <p><strong>Preço por noite:</strong> R$ {casa.preco}</p>      
-      <p>Se quer reservar, clique <Link to={`/reserva/${casa.id}`}>Aqui</Link>.</p>
+      <div className="cadastro-box">
+        {registroData.dataIni || registroData.dataFim && <h3><strong>Adiciode as datas para ver o preço</strong></h3>}
+        {registroData.dataIni && registroData.dataFim && <h3><strong>R$ {precoFinal.toFixed(2)}</strong> por {periodo} noites</h3>}
+        <form onSubmit={handleSubmit} className="cadastroForm">
+          <div className="form-group">
+            <label htmlFor="inicio">Check-in
+              <input 
+                type="date"
+                id="checkIn"
+                name="dataIni"
+                value={registroData.dataIni} 
+                onChange={handleChange}
+                className={errors.dataIni ? 'error' : ''}
+              />
+            </label>
+            {errors.dataIni && <span className="error-message">{errors.dataIni}</span>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="fim">Check-out
+              <input 
+                type="date"
+                id="checkOut"
+                name="dataFim"
+                value={registroData.dataFim}
+                onChange={handleChange}
+                className={errors.dataFim ? 'error' : ''}
+              />
+            </label>
+            {errors.dataFim && <span className="error-message">{errors.dataFim}</span>}
+          </div>
+          <div className="form-group">
+            <label htmlFor="hospedes">
+              <input 
+                type="number"
+                id="hospedes"
+                min={1}
+                name="hospedes"
+                value={registroData.hospedes}
+                onChange={handleChange}
+                className={errors.hospedes ? 'error' : ''}
+              />
+            </label>
+            {errors.hospedes && <span className="error-message">{errors.hospedes}</span>}
+          </div>
+          <button type="submit" className="submit-button"> Fazer Reserva </button>
+        </form>
+      </div>      
     </div>
   );
 }
